a483c0be48067d0d752dac9be1805e0fce14d73b
nurodev <me@nuro.dev>
2022-11-24 16:11:28 +0000

    Improved build config for module structure

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,33 +1,47 @@
 import { defineConfig } from "tsup";
 
+import type { Options } from "tsup";
+
 const isProduction = process.env.NODE_ENV === "production";
 
-export default defineConfig({
+const baseConfigOptions: Options = {
   clean: true,
   dts: true,
-  entryPoints: {
-    index: "src/index.ts",
-    modules: "src/modules/index.ts",
-    types: "src/types/index.ts",
-    ...Object.fromEntries(
-      [
-        "checkout",
-        "discount",
-        "file",
-        "licenseKey",
-        "licenseKeyInstance",
-        "order",
-        "orderItem",
-        "product",
-        "store",
-        "subscription",
-        "user",
-        "variant",
-      ].map((module) => [module, `src/modules/${module}/index.ts`])
-    ),
-  },
   format: ["cjs", "esm"],
   minify: isProduction,
   sourcemap: true,
   splitting: false,
-});
+};
+
+export default defineConfig([
+  {
+    ...baseConfigOptions,
+    entryPoints: {
+      index: "src/index.ts",
+      types: "src/types/index.ts",
+    },
+  },
+  {
+    ...baseConfigOptions,
+    entry: {
+      index: "src/modules/index.ts",
+      ...Object.fromEntries(
+        [
+          "checkout",
+          "discount",
+          "file",
+          "licenseKey",
+          "licenseKeyInstance",
+          "order",
+          "orderItem",
+          "product",
+          "store",
+          "subscription",
+          "user",
+          "variant",
+        ].map((module) => [module, `src/modules/${module}/index.ts`])
+      ),
+    },
+    outDir: "dist/modules",
+  },
+]);

c5b80cf3e85d3af51130b70b78a2050b36c1490d
nurodev <me@nuro.dev>
2023-07-03 09:32:32 +0100

    Formatted Prettier config

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,16 +1,14 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-	arrowParens: "always",
-	bracketSameLine: false,
-	bracketSpacing: true,
-	importOrderSeparation: true,
-	plugins: [
-		require.resolve("@trivago/prettier-plugin-sort-imports"),
-	],
-	printWidth: 100,
-	semi: true,
-	singleQuote: true,
-	tabWidth: 4,
-	trailingComma: "all",
-	useTabs: true
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  importOrderSeparation: true,
+  plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: "all",
+  useTabs: true,
 };

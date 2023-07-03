6c515b23bea8d0a145ffd34bdac917e7949dcb9d
nurodev <me@nuro.dev>
2023-07-03 09:31:17 +0100

    Added initial Prettier config

---
+++ .prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	arrowParens: "always",
+	bracketSameLine: false,
+	bracketSpacing: true,
+	importOrderSeparation: true,
+	printWidth: 100,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: "all",
+	useTabs: true
+};
